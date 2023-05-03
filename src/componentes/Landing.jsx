9c437b0e3cac46970bda9ac0a81536846c352193
Hernan bellassai <hernandario44@hotmail.com>
2023-05-03 15:11:41 -0300

    nuevo comit

--- src/componentes/Landing.jsx
+++ src/componentes/Landing.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import "../styles/Landing.css";
+import { Player } from "@lottiefiles/react-lottie-player";
 
 function Landing() {
+  
+  
+  useEffect(()=>{
+    
+
+  },[]);
+  
+  
+  
   return (
-    <div>Landing</div>
-  )
+    <div className="componente">
+      <h1 className="logo">hundy</h1>
+
+        
+   
+       
+      <div>
+        {" "}
+        <Player
+          src="https://assets5.lottiefiles.com/packages/lf20_sYVZ3n.json"
+          loop
+          autoplay
+          style={{ height: "500px", width: "500px" }}
+        />
+      </div>
+    </div>
+  );
 }
 
-export default Landing
+export default Landing;
